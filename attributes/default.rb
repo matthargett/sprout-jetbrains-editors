727ea0aa532edabd478c70623991e4de3245cf86
Luke Winikates <luke.winikates@gmail.com>
2015-01-01 17:24:09 -0800

    clone the pivotal_ide_prefs repo into ~/workspace
      (instead of the Chef file cache)

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,6 +2,6 @@ default['sprout']['jetbrains-editors']['preferences']['repository'] =
   'https://github.com/pivotal/pivotal_ide_prefs.git'
 
 default['sprout']['jetbrains-editors']['preferences']['clone_dir'] =
-  "#{Chef::Config[:file_cache_path]}/pivotal_ide_prefs"
+  "#{node['sprout']['home']}/workspace/pivotal_ide_prefs"
 
 default['sprout']['jetbrains-editors']['preferences']['revision'] = 'master'
