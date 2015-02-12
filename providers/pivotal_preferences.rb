3fc4a6f48e60f8214f0fa788dd3f32e81267d7c7
Abhi Hiremagalur <abhih@pivotal.io>
2015-02-12 15:27:36 -0800

    Missed another reference to current_user

--- providers/pivotal_preferences.rb
+++ providers/pivotal_preferences.rb
@@ -6,7 +6,7 @@ action :install do
   execute 'install_intellij_prefs' do
     cwd preferences['clone_dir'] + '/cli'
     command "bin/ide_prefs --ide=#{package} install"
-    user node['current_user']
+    user node['sprout']['user']
   end
 
   new_resource.updated_by_last_action(true)
